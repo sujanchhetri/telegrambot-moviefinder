2182519816a7ab3684d087d93a42fd06a7e0b93d
Sujan Chhetri <44917558+sujanchhetri@users.noreply.github.com>
2020-11-29 13:31:31 +0545

    initial

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(process.env.BOT_APIKEY, { polling: true });
+const bot = new TelegramBot('BOT_APIKEY', { polling: true });
 const request = require('request');
 const express = require('express');
 const app = express();
@@ -10,7 +10,7 @@ require('dotenv').config();
 bot.onText(/\/movie (.+)/, (msg, match) => {
     let movie = match[1];
     let chatId = msg.chat.id;
-    request(`http://www.omdbapi.com/?apiKey=${process.env.BOT_APIKEY.OMDAPIKEY}=${movie}`, (error, response, body) => {
+    request(`http://www.omdbapi.com/?apiKey=OMDAPIKEY=${movie}`, (error, response, body) => {
         if (!error && response.statusCode == 200) {
             bot.sendMessage(chatId, '_Looking for _' + movie + '...', { parse_mode: 'Markdown' })
                 .then((msg) => {
